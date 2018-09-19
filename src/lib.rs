daa79616bef86847aa1b7fc6175c66d3743fa103
Josh Leeb-du Toit <josh.leebdutoit@gmail.com>
2018-09-19 18:49:25 +1000

    Implement template_decl params

--- src/lib.rs
+++ src/lib.rs
@@ -23,18 +23,31 @@ mod tests {
 
     #[test]
     fn template_decl_no_parameters() {
-        let parsed = parse(
-            Rule::template,
-            "tmpl_ident ::= <<\n\
-             tmpl content\n\
-             >>",
-        );
+        let parsed = parse(Rule::template_decl, "T ::= <<");
+        assert_eq!(parsed, [(Rule::ident, "T")]);
+    }
+
+    #[test]
+    fn template_decl_one_parameter() {
+        let parsed = parse(Rule::template_decl, "T(p1) ::= <<");
+        assert_eq!(parsed, [(Rule::ident, "T"), (Rule::param, "p1"),]);
+    }
+
+    #[test]
+    fn template_decl_multiple_parameters() {
+        let parsed = parse(Rule::template_decl, "T(p1, p2) ::= <<");
         assert_eq!(
             parsed,
-            [
-                (Rule::ident, "tmpl_ident"),
-                (Rule::content, "tmpl content\n")
-            ]
+            [(Rule::ident, "T"), (Rule::param, "p1"), (Rule::param, "p2")]
+        );
+    }
+
+    #[test]
+    fn template_no_parameters() {
+        let parsed = parse(Rule::template, "T ::= <<\ncontent\n>>");
+        assert_eq!(
+            parsed,
+            [(Rule::ident, "T"), (Rule::template_content, "content\n")]
         );
     }
 }
