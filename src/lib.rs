e085fa4d9f75bae47572350594c06b57209b7962
Josh Leeb-du Toit <josh.leebdutoit@gmail.com>
2018-09-19 23:49:11 +1000

    Complete grammar for parsing group templates

--- src/lib.rs
+++ src/lib.rs
@@ -5,11 +5,11 @@ use pest_derive::*;
 // Forces the grammar to be rebuilt when compiling in debug mode. For more info see
 // https://docs.rs/pest_derive/1.0.8/pest_derive/#pest-files
 #[cfg(debug_assertions)]
-const _GRAMMAR: &'static str = include_str!("grammar.pest");
+const _GRAMMAR: &'static str = include_str!("grammars/group.pest");
 
 #[derive(Parser)]
-#[grammar = "grammar.pest"]
-pub struct TemplateParser;
+#[grammar = "grammars/group.pest"]
+pub struct GroupParser;
 
 #[cfg(test)]
 mod tests {
@@ -17,25 +17,31 @@ mod tests {
     use pest::Parser;
 
     fn parse(rule: Rule, content: &str) -> Vec<(Rule, &str)> {
-        let pairs = TemplateParser::parse(rule, content).unwrap();
+        let pairs = GroupParser::parse(rule, content).unwrap();
         pairs.map(|pair| (pair.as_rule(), pair.as_str())).collect()
     }
 
     #[test]
-    fn template_decl_no_parameters() {
-        let parsed = parse(Rule::template_decl, "T ::= <<");
+    fn decl_no_parameters() {
+        let parsed = parse(Rule::decl, "T ::= <<");
         assert_eq!(parsed, [(Rule::ident, "T")]);
     }
 
     #[test]
-    fn template_decl_one_parameter() {
-        let parsed = parse(Rule::template_decl, "T(p1) ::= <<");
+    fn decl_empty_parameters() {
+        let parsed = parse(Rule::decl, "T() ::= <<");
+        assert_eq!(parsed, [(Rule::ident, "T")]);
+    }
+
+    #[test]
+    fn decl_one_parameter() {
+        let parsed = parse(Rule::decl, "T(p1) ::= <<");
         assert_eq!(parsed, [(Rule::ident, "T"), (Rule::param, "p1"),]);
     }
 
     #[test]
-    fn template_decl_multiple_parameters() {
-        let parsed = parse(Rule::template_decl, "T(p1, p2) ::= <<");
+    fn decl_multiple_parameters() {
+        let parsed = parse(Rule::decl, "T(p1, p2) ::= <<");
         assert_eq!(
             parsed,
             [(Rule::ident, "T"), (Rule::param, "p1"), (Rule::param, "p2")]
@@ -43,11 +49,37 @@ mod tests {
     }
 
     #[test]
-    fn template_no_parameters() {
-        let parsed = parse(Rule::template, "T ::= <<\ncontent\n>>");
+    fn basic_template() {
+        let parsed = parse(Rule::group, "T ::= <<content>>");
+        assert_eq!(parsed, [(Rule::ident, "T"), (Rule::template, "content")]);
+    }
+
+    #[test]
+    fn empty_template() {
+        let parsed = parse(Rule::group, "T ::= <<>>");
+        assert_eq!(parsed, [(Rule::ident, "T")]);
+    }
+
+    #[test]
+    fn escaped_template() {
+        let parsed = parse(Rule::group, "T ::= <<\\<<content\\>>>>");
         assert_eq!(
             parsed,
-            [(Rule::ident, "T"), (Rule::template_content, "content\n")]
+            [(Rule::ident, "T"), (Rule::template, "\\<<content\\>>")]
+        );
+    }
+
+    #[test]
+    fn group_file() {
+        let parsed = parse(Rule::file, "A ::= <<c_A>>\nB ::= <<c_B>>");
+        assert_eq!(
+            parsed,
+            [
+                (Rule::ident, "A"),
+                (Rule::template, "c_A"),
+                (Rule::ident, "B"),
+                (Rule::template, "c_B")
+            ]
         );
     }
 }
